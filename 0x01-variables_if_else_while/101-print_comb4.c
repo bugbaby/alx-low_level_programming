bff13396ea5fae7e60eb1b833e23c34f16dc7234
bugbaby <joyeneh234@gmail.com>
2022-09-12 03:10:01 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -28,16 +28,15 @@ int main(void)
 										putchar(n2);
 										putchar(n3);
 										if (n1 != 55 || n2 != 56 || n3 != 57)
-										{
-															putchar(com);
-															putchar(32);
-														}
-										n3 += 1;
-									}
-						n2 += 1;
-					}
-			n1 += 1;
+										{									putchar(com);
+									     	   putchar(32);
+												}
+	                									n3 += 1;
+			}
+	        	n2 += 1; 			
 		}
+		n1 += 1;
+	}
 	putchar('\n');
 	return (0);
-}}
+}
