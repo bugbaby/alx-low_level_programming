c049e1106e07466c4f59705d4c8e4797273a01cb
bugbaby <joyeneh234@gmail.com>
2022-09-14 11:54:25 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+#include <stdio>
 
 /**
- *  * main - list all natural numbers multiples of 3 & 5
- *   *
- *    * Return: Always 0.
- *     */
+ * main - list all natural numbers multiples of 3 & 5
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
-		int num, sum;
+	int num, sum;
 
-			sum = 0;
-
-				for (num = 0; num < 1024; num++)
-						{
-									if (num % 3 == 0 || num % 5 == 0)
-												{
-																sum += num;
-																		}
-										}
-					printf("%d\n", sum);
-						return (0);
+	sum = 0;
+	
+	for (num = 0; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
