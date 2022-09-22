faa44827fadf14f85cb70a10dc655d8f433ffa9a
bugbaby <joyeneh234@gmail.com>
2022-09-22 13:19:28 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,14 +3,10 @@
 
 
 /**
- *
- *  * print_number - prints an integer;
- *
- *   * @n: integer to be printed;
- *
- *    * Return: Always 0
- *
- *     */
+ * print_number - prints an integer;
+ * @n: integer to be printed;
+ * Return: Always 0
+ */
 
 void print_number(int n)
 
