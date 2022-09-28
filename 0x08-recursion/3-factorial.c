cee2673a2e26ef32065a09eeb795104e8a395b8e
bugbaby <joyeneh234@gmail.com>
2022-09-28 10:47:50 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * fctorial - returns the factorial of a given number.
+ * factorial - returns the factorial of a given number.
  * @n: input number.
  * Return: factorial of the number
  */
